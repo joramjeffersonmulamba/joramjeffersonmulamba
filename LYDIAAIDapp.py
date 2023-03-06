f5091d4af83f05ac24306ab33f18e94f2c7d0832
joramjeffersonmulamba <99241136+joramjeffersonmulamba@users.noreply.github.com>
2023-03-06 16:57:05 +0300

    Update LYDIAAIDapp.py

--- LYDIAAIDapp.py
+++ LYDIAAIDapp.py
@@ -17,42 +17,6 @@ import h5py
 from img_classification import teachable_machine_classification
 
 
-@st.cache(allow_output_mutation=True)
-def teachable_machine_classification(img_path, weights_file):
-    image = Image.open(image_path)
-    # Load the model
-    model_path = os.path.join(os.getcwd(), weights_file)
-    model = keras.models.load_model(model_path)
-    img = keras.preprocessing.image.img_to_array(image)
-    img = tf.keras.preprocessing.image.smart_resize(img, (224, 224))
-    img = tf.reshape(img, (-1, 224, 224, 3))
-    img /= 255.0
-    
-    # Create the array of the right shape to feed into the keras model
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    image = img
-    #image sizing
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-    #turn the image into a numpy array
-    image_array = np.asarray(image)
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    # Ensure that the image has 3 channels (if not grayscale)
-    if len(normalized_image_array.shape) == 2:
-        normalized_image_array = np.stack(
-            [normalized_image_array] * 3, axis=-1)
-    elif normalized_image_array.shape[-1] == 4:
-        normalized_image_array = normalized_image_array[:, :, :3]
-
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run the inference
-    prediction = model.predict(data)
-    return np.argmax(prediction) # return position of the highest probability
 
 
 
