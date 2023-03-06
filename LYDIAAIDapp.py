dee05196eec6b76a012403a5726893775562a0ac
joramjeffersonmulamba <99241136+joramjeffersonmulamba@users.noreply.github.com>
2023-03-06 19:23:13 +0300

    Update LYDIAAIDapp.py

--- LYDIAAIDapp.py
+++ LYDIAAIDapp.py
@@ -27,25 +27,32 @@ def teachable_machine_classification(file, weights_url):
     # Load the model
     model = keras.models.load_model(weights_file)
 
-    # Load the image from the UploadedFile object
-    img = Image.open(file).convert('RGB')
+    # Create the array of the right shape to feed into the keras model
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    image = img
+    #image sizing
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
-    # Convert the image to a Numpy array
-    img_array = img_to_array(img)
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
-    # Expand the dimensions of the image to match the input shape of the model
-    img_array = np.expand_dims(img_array, axis=0)
+    # Ensure that the image has 3 channels (if not grayscale)
+    if len(normalized_image_array.shape) == 2:
+        normalized_image_array = np.stack(
+            [normalized_image_array] * 3, axis=-1)
+    elif normalized_image_array.shape[-1] == 4:
+        normalized_image_array = normalized_image_array[:, :, :3]
 
-    # Preprocess the input image
-    img_array = keras.applications.mobilenet_v2.preprocess_input(img_array)
+    # Load the image into the array
+    data[0] = normalized_image_array
 
-    # Make predictions
-    predictions = model.predict(img_array)
-
-    # Get the predicted class label
-    label = np.argmax(predictions)
-
-    return label
+    # run the inference
+    prediction = model.predict(data)
+    return np.argmax(prediction) # return position of the highest probability
+   
 
 # Load the data
 data = pd.read_csv("https://github.com/joramjeffersonmulamba/joramjeffersonmulamba/blob/master/data.csv?raw=true")
