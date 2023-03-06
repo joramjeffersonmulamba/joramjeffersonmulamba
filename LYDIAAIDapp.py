b57878c607ef2306b7ccfbd7249827784c91993c
joramjeffersonmulamba <99241136+joramjeffersonmulamba@users.noreply.github.com>
2023-03-06 18:11:15 +0300

    Update LYDIAAIDapp.py

--- LYDIAAIDapp.py
+++ LYDIAAIDapp.py
@@ -212,7 +212,7 @@ if uploaded_file is not None:
 
 st.title("Teachable Machine Image Classification")
 st.markdown("Upload an image to classify the image")
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image...", type="png")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
