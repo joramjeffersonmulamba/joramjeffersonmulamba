bb6b9d3d72a870862186b294b6e240a29b9ba9bd
joramjeffersonmulamba <99241136+joramjeffersonmulamba@users.noreply.github.com>
2023-03-06 16:59:46 +0300

    Update LYDIAAIDapp.py

--- LYDIAAIDapp.py
+++ LYDIAAIDapp.py
@@ -14,9 +14,30 @@ import tempfile
 import os
 import urllib.request
 import h5py
-from img_classification import teachable_machine_classification
 
 
+def teachable_machine_classification(img, weights_file):
+    # Load the model
+    model = keras.models.load_model(weights_file)
+
+    # Create the array of the right shape to feed into the keras model
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    image = img
+    #image sizing
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    return np.argmax(prediction) # return position of the highest probability
 
 
 
@@ -211,7 +232,7 @@ if uploaded_file is not None:
     st.image(image, caption='Uploaded Scan.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
-    label = teachable_machine_classification(image, 'model/keras_model.h5')
+    label = teachable_machine_classification(image, 'https://github.com/joramjeffersonmulamba/joramjeffersonmulamba/blob/master/keras_model2.h5?raw=true')
     if label == 0:
         st.write("The scan is normal")
     elif label == 1:
