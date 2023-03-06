f1ce7aee0b55926eaf756201687d0fb1ef7e6698
joramjeffersonmulamba <99241136+joramjeffersonmulamba@users.noreply.github.com>
2023-03-06 15:59:04 +0300

    Update LYDIAAIDapp.py

--- LYDIAAIDapp.py
+++ LYDIAAIDapp.py
@@ -243,7 +243,7 @@ urllib.request.urlretrieve(url, "keras_model3.h5")
 
 
 
-uploaded_file3 = st.file_uploader("Choose a scan ...", type=["png", "jpeg", "jpg"] key="file3")
+uploaded_file3 = st.file_uploader("Choose a scan ...", type="png", key="file3")
 if uploaded_file3 is not None:
     image = Image.open(uploaded_file3)
     st.image(image, caption='Uploaded Scan.', use_column_width=True)
